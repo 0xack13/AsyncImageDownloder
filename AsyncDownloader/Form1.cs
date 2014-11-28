5a312ec7d81d2dc50fcba0d77a4cf3ac11c75ba4
0xack13 <0xack13@gmail.com>
2014-11-29 00:09:06 +0300

    Download Google logo in a blocking way

--- AsyncDownloader/Form1.cs
+++ AsyncDownloader/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,18 @@ namespace AsyncDownloader
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WebClient client = new WebClient();
+            byte[] imageData = client.DownloadData("https://www.google.com.sa/images/srpr/logo11w.png");
+            this.pictureBox1.Image = Image.FromStream(new MemoryStream(imageData));
         }
     }
 }
