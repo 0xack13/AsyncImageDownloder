1305c9a2057fac380a992caf659b98005ddffbca
0xack13 <0xack13@gmail.com>
2014-11-30 17:00:53 +0300

    Yield example

---
+++ YieldSample/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YieldSample
+{
+    class Program
+    {
+        static List<int> MyList = new List<int>();
+
+        static void FillValues()
+        {
+            MyList.Add(1);
+            MyList.Add(2);
+            MyList.Add(3);
+            MyList.Add(4);
+            MyList.Add(5);
+        }
+        static void Main(string[] args)
+        {
+            FillValues();
+            foreach (int i in RunningTotal())
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine(); 
+        }
+
+        static IEnumerable<int> RunningTotal()
+        {
+            int runningtotal = 0;
+            foreach (int i in MyList)
+            {
+                runningtotal += i;
+                yield return (runningtotal);
+
+            }
+        }
+    }
+}
